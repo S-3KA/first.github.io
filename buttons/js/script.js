e2965456bb3b9ba8e23a93a4439ae0f88d4925ef
S-3KA <84847251+S-3KA@users.noreply.github.com>
2021-05-26 23:00:21 +0600

    Add files via upload

---
+++ buttons/js/script.js
@@ -0,0 +1,36 @@
+"use stroct"
+
+let getElemetId1 = document.getElementById("prompt_elem1")
+let getElemetId2 = document.getElementById("prompt_elem2")
+let getElemetId3 = document.getElementById("prompt_elem3")
+let getElemetId4 = document.getElementById("prompt_elem4")
+let getElemetId5 = document.getElementById("prompt_elem5")
+
+function swap1(){
+    getElemetId1.classList.toggle("visable");
+    getElemetId1.classList.toggle("zindex");
+
+}
+
+function swap2(){
+    getElemetId2.classList.toggle("visable");
+    getElemetId2.classList.toggle("zindex");
+
+}
+
+function swap3(){
+    getElemetId3.classList.toggle("visable");
+    getElemetId3.classList.toggle("zindex");
+
+}
+
+function swap4(){
+    getElemetId4.classList.toggle("visable");
+    getElemetId4.classList.toggle("zindex");
+
+}
+function swap5(){
+    getElemetId5.classList.toggle("visable");
+    getElemetId5.classList.toggle("zindex");
+
+}
--- js/script.js
+++ js/script.js
@@ -1,36 +1,12 @@
-"use stroct"
+"use strict"
 
-let getElemetId1 = document.getElementById("prompt_elem1")
-let getElemetId2 = document.getElementById("prompt_elem2")
-let getElemetId3 = document.getElementById("prompt_elem3")
-let getElemetId4 = document.getElementById("prompt_elem4")
-let getElemetId5 = document.getElementById("prompt_elem5")
+let menuIcon = document.getElementById("open_menu")
+let getMenu = document.getElementById("menu")
 
-function swap1(){
-    getElemetId1.classList.toggle("visable");
-    getElemetId1.classList.toggle("zindex");
+function open__menu(){
+    menuIcon.classList.toggle("icon_open");
+    menuIcon.classList.toggle("icon_close");
 
-}
-
-function swap2(){
-    getElemetId2.classList.toggle("visable");
-    getElemetId2.classList.toggle("zindex");
-
-}
-
-function swap3(){
-    getElemetId3.classList.toggle("visable");
-    getElemetId3.classList.toggle("zindex");
-
-}
-
-function swap4(){
-    getElemetId4.classList.toggle("visable");
-    getElemetId4.classList.toggle("zindex");
-
-}
-function swap5(){
-    getElemetId5.classList.toggle("visable");
-    getElemetId5.classList.toggle("zindex");
-
+    getMenu.classList.toggle("menu_open");
+    getMenu.classList.toggle("menu_close");
 }
