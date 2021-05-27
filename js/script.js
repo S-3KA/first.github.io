63ac0ff2b1230604860cca6ff653d426d8fcbbe1
S-3KA <84847251+S-3KA@users.noreply.github.com>
2021-05-27 23:46:10 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 let menuIcon = document.getElementById("open_menu")
 let getMenu = document.getElementById("menu")
 
+let getWelcome = document.getElementById("hi")
+
 function open__menu(){
     menuIcon.classList.toggle("icon_open");
     menuIcon.classList.toggle("icon_close");
@@ -10,3 +12,16 @@ function open__menu(){
     getMenu.classList.toggle("menu_open");
     getMenu.classList.toggle("menu_close");
 }
+
+setTimeout(function(){
+    getWelcome.classList.toggle("hi_hide")
+
+    setTimeout(function(){
+        document.getElementsByClassName("hi_outLine")[0].style.width = "100%"
+    },1400)
+    setTimeout(function(){
+        document.getElementsByClassName("hi_outLine")[0].style.margin = "0 0 0 100%"
+        document.getElementsByClassName("hi_outLine")[0].style.width = "0px"
+    },2700)
+},20)
+
