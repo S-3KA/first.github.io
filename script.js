ed18b348f557e54e20facee6f8c8e2b502e5d31a
S-3KA <84847251+S-3KA@users.noreply.github.com>
2021-05-28 16:10:57 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict"
+
+let menuIcon = document.getElementById("open_menu")
+let getMenu = document.getElementById("menu")
+
+let getWelcome = document.getElementById("hi")
+
+function open__menu(){
+    menuIcon.classList.toggle("icon_open");
+    menuIcon.classList.toggle("icon_close");
+
+    getMenu.classList.toggle("menu_open");
+    getMenu.classList.toggle("menu_close");
+}
+
+function screenLoad(){
+    setTimeout(function(){
+        getWelcome.classList.toggle("hi_hide");
+    
+        setTimeout(function(){
+            document.getElementsByClassName("hi_outLine")[0].style.width = "100%";
+        },1400)
+        setTimeout(function(){
+            document.getElementsByClassName("hi_outLine")[0].style.margin = "0 0 0 100%";
+            document.getElementsByClassName("hi_outLine")[0].style.width = "0px";
+        },2700)
+    },2)
+}
+
+document.addEventListener("DOMContentLoaded", screenLoad);
