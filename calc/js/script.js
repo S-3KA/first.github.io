0ee250eca195dbb1969b8cfba73ea4d6e7635635
S-3KA <84847251+S-3KA@users.noreply.github.com>
2021-05-29 08:14:31 +0600

    Add files via upload

--- calc/js/script.js
+++ calc/js/script.js
@@ -1,34 +1,15 @@
 "use strict"
-let signs = [
-    '1', '2', '3', '+',
-    '4', '5', '6', '-',
-    '7', '8', '9', '/',
-    '0', '=', '.', 'c',
-];
 
-let calc = document.getElementById('calc');
-let contButtons = document.getElementById("buttons")
-let textArea = document.getElementById('inputVal');
-
-signs.forEach(function (sign) {
-    let signElement = document.createElement('div');
-    signElement.className = 'btn';
-    signElement.innerHTML = sign;
-    contButtons.appendChild(signElement);
-});
-
-document.querySelectorAll('.btn').forEach(function (button) {
-    button.addEventListener('click', onButtonClick);
-});
-
-function onButtonClick(e) {
-    if (e.target.innerHTML === 'c') {
-        textArea.innerHTML = '0';
-    } else if (e.target.innerHTML === '=') {
-        textArea.innerHTML = eval(textArea.innerHTML);
-    } else if (textArea.innerHTML === '0') {
-        textArea.innerHTML = e.target.innerHTML;
-    } else {
-        textArea.innerHTML += e.target.innerHTML;
+let calcValue = document.getElementById("calcu").innerHTML;
+ 
+function fclick(){
+    if (event.target.innerHTML === "="){
+        // alert(eval(calcValue));
+        calcValue = (eval(document.getElementById("calcu").innerHTML))
+        calcu.innerHTML = calcValue
+    }else if (event.target.innerHTML === "c"){
+        calcu.innerHTML = ""
+    }else {
+        calcu.innerHTML += event.target.innerHTML
     }
 }
